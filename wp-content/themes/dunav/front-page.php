ab50c61b34fe79c177d4339ca209ac6ed720840f
Marijana <Marijana@remote1>
2019-01-10 13:06:56 +0100

    core template

---
+++ wp-content/themes/dunav/front-page.php
@@ -0,0 +1,19 @@
+<?php 
+get_header();
+?>
+
+<!--I WANT TO SY SOMETHING-->
+<header>
+    <h1>
+        <?php bloginfo('name'); ?>
+    </h1>
+    <h3>
+        <?php bloginfo('description'); ?>
+    </h3>
+</header>
+
+ 
+
+<?php
+get_footer();
+?>
